0c3ec09d4c572085e374f9abb1b2ebb9e8f7e1fe
Vladyslav Natalych <v.natalich05@gmail.com>
2024-11-26 22:16:08 +0200

    Fixing fetching error

--- src/apiService/fetchCardData.ts
+++ src/apiService/fetchCardData.ts
@@ -1,16 +1,16 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_KEY = '33J-hHOVDtSzhZefII_e_EYyB10rzckrD6pig0C11YI';
-axios.defaults.baseURL = 'https://api.unsplash.com/';
-axios.defaults.headers.common['Authorization'] = `Client-ID ${API_KEY}`;
+const API_KEY = "33J-hHOVDtSzhZefII_e_EYyB10rzckrD6pig0C11YI";
+axios.defaults.baseURL = "https://api.unsplash.com/";
+axios.defaults.headers.common["Authorization"] = `Client-ID ${API_KEY}`;
 
 axios.defaults.params = {
-  orientation: 'landscape',
+  orientation: "landscape",
   per_page: 24,
 };
 
-export const fetchPhotos = async (param, page) => {
+// Explicitly define the types for the parameters
+export const fetchPhotos = async (param: string, page: number) => {
   const { data } = await axios.get(`search/photos?query=${param}&page=${page}`);
-
   return data;
 };
